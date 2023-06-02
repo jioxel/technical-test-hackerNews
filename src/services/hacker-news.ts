8e7194f44dd2be43c42ecab5567433519cd0316e
Jovany <jovanyevs@gmail.com>
2023-06-02 16:05:17 -0600

    Services

---
+++ src/services/hacker-news.ts
@@ -0,0 +1,13 @@
+export const getTopStories = async (page: number, limit: number) => {
+     const responce = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
+     const json = await responce.json()
+     // page start with 1
+     const startIndex = (page - 1) * limit
+     const endIndex = startIndex + limit
+     const ids = json.slice(startIndex, endIndex)
+     return ids
+}
+export const getItemInfo = async (id: number) => {
+     const responce = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
+     return await responce.json()
+}
